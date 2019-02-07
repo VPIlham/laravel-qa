b1edd95fd7f2e1a24cabae4e6f251187f473ac46
Ilham Nur Hakim <ilhamnurhakim378@gmail.com>
2019-02-07 16:19:14 +0700

    menambahkan author info dan pertanyaan created date

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -11,7 +11,14 @@
                     @foreach ($questions as $quest)
                       <div class="media">
                           <div class="media-body">
-                            <h3 class="mt-0">{{ $quest->title }}</h3>
+                            <h3 class="mt-0">
+                              <a href="{{ $quest->url }}">{{ $quest->title }}</a>
+                            </h3>
+                            <p class="lead">
+                              Asked By 
+                              <a href="{{ $quest->user->url }}"> {{$quest->user->name}} </a>
+                              <small class="text-muted">{{ $quest->created_date }}</small>
+                            </p>
                             {{ str_limit($quest->body,250) }}
                           </div>
                       </div>    
